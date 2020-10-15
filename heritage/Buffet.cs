ae30033e21794aa03d383dfc925726bda8aa16d6
Norro valentin <norrova@gmail.com>
2020-10-15 17:08:11 +0200

    add heritage

---
+++ heritage/Buffet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace heritage
+{
+    public class Buffet : Meuble
+    {
+        public Buffet(double p_hauteur, double p_largeur, double p_longueur) : base(p_hauteur, p_largeur, p_longueur)
+        {
+        }
+
+        public override string NoticeDeMontage()
+        {
+            return $"Ne pas utiliser le chat comme soutien de porte !";
+        }
+    }
+}
---
+++ heritage/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace heritage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ikea v_ikea = new Ikea("Lille", new List<Meuble>() {
+                new Table(3.43, 23.23, 12.00),
+                new Bureau(3.43, 23.23, 12.00),
+                new Buffet(3.43, 23.23, 12.00)
+            });
+            Console.WriteLine(v_ikea.ListerLesProduits());
+        }
+    }
+}
