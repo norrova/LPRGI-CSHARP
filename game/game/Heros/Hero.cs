4bee30c3c04c960b3cc81395d9093fa1d1faaf4d
Norro valentin <norrova@gmail.com>
2020-10-19 18:30:09 +0200

    rm others filess

--- game/game/Heros/Hero.cs
+++ game/game/Heros/Hero.cs
@@ -3,7 +3,7 @@ namespace game.Heros
 {
     public class Hero : Pnj
     {        
-        private int m_monsterKilled = 0; 
+        private int m_monsterKilled; 
         public int MonsterKilled { get => m_monsterKilled; set => m_monsterKilled = value; }
 
         public Hero(int p_lifePoint, int p_attackValue) : base(p_lifePoint, p_attackValue)
@@ -13,7 +13,7 @@ namespace game.Heros
 
         public Hero(int p_lifePoint, int p_attackValue, int p_monsterKilled) : base(p_lifePoint, p_attackValue)
         {
-            MonsterKilled = IsPositive(p_monsterKilled);
+            MonsterKilled = p_monsterKilled >= 0 ? p_monsterKilled : 0;
         }
     }
 }
--- game/game/Program.cs
+++ game/game/Program.cs
@@ -7,7 +7,7 @@ namespace game
     {
         static void Main(string[] args)
         {
-            Hero v_hero = new Hero(500, 70);
+            Hero v_hero = new Hero(100, 70);
             GestionnaireDePartie v_game = new GestionnaireDePartie(v_hero);
             v_game.Play();
             v_game.ShowScore();
