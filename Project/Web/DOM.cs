e37dd0c4368c5f18e3c9af128a5baff2f2a17e87
Norro valentin <norrova@gmail.com>
2020-11-13 13:11:39 +0100

    update

---
+++ Project/Web/DOM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Project.Web
+{
+    public static class DOM
+    {
+        private static readonly String P = "p";
+
+        public static String GetParagraph(this String p_string)
+        {
+            if (String.IsNullOrEmpty(p_string)) throw new ArgumentNullException();
+            return CreateElement(P, p_string);
+        }
+
+        private static String CreateElement(String p_tag, String p_string)
+        {
+            return $"<{p_tag}>{p_string}</{p_tag}>";
+        }
+    }
+}
